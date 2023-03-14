70011de97bcdcea874a93ce871eb829462d95eda
Kakeshin <kakeshin.engineer@gmail.com>
2023-03-15 02:19:35 +0900

    add

--- src/component/pages/Menu.tsx
+++ src/component/pages/Menu.tsx
@@ -1,4 +1,16 @@
-import { Image, HStack, VStack, Heading, Stack } from "@chakra-ui/react";
+import {
+  Image,
+  Stack,
+  HStack,
+  VStack,
+  Heading,
+  Card,
+  CardHeader,
+  CardBody,
+  Box,
+  Text,
+  StackDivider,
+} from "@chakra-ui/react";
 import SideNav from "./SideNav";
 
 const Menu = () => {
@@ -8,12 +20,56 @@ const Menu = () => {
         <Image src="src/assets/logo_only.png" w="4%" />
         <Heading color="#74AECB">KAKESHIN LEGENDS</Heading>
       </HStack>
-      <HStack ml="1%">
+      <HStack>
         <SideNav />
-        <VStack>
-          <Heading color="gray.400" bg="white" fontSize="2xl">
-            会社情報
-          </Heading>
+        <VStack bg="white" color="gray.400">
+          <Card>
+            <CardHeader>
+              <Heading size="md">My Profile</Heading>
+            </CardHeader>
+            <CardBody>
+              <Stack divider={<StackDivider />} spacing="4">
+                <Box>
+                  <Heading size="xs" textTransform="uppercase">
+                    name
+                  </Heading>
+                  <Text pt="2" size="sm">
+                    Kakeshin
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size="xs" textTransform="uppercase">
+                    age
+                  </Heading>
+                  <Text pt="2" size="sm">
+                    Unknown
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size="xs" textTransform="uppercase">
+                    hometown
+                  </Heading>
+                  <Text pt="2" size="sm">
+                    Hokkaido
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size="xs" textTransform="uppercase">
+                    sns
+                  </Heading>
+                  <Text pt="2" size="sm">
+                    Twitter
+                  </Text>
+                  <Text pt="2" size="sm">
+                    Instagram
+                  </Text>
+                  <Text pt="2" size="sm">
+                    GitHub
+                  </Text>
+                </Box>
+              </Stack>
+            </CardBody>
+          </Card>
         </VStack>
       </HStack>
     </VStack>
